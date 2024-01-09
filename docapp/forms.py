fb9a0dcfa48063686e794dcb61f0ba5e2f558966
ateeb <ateebnaveed1996@gmail.com>
2024-01-09 20:10:01 +0500

    Some changes to fend

--- docapp/forms.py
+++ docapp/forms.py
@@ -3,18 +3,17 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserFrom(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-    # address = forms.CharField(max_length=250)
+    # email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    # first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
     
-    def __init__(self, *args, **kwargs):
-        super(RegisterUserFrom, self).__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super(RegisterUserFrom, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
+    #     self.fields['username'].widget.attrs['class'] = 'form-control'
+    #     self.fields['password1'].widget.attrs['class'] = 'form-control'
+    #     self.fields['password2'].widget.attrs['class'] = 'form-control'
